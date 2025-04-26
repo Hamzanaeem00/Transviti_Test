db770ead18dffeb39f5583e1cb07f064b6f06365
Hamzanaeem00 <hamzanaeemkhan977@gmail.com>
2025-04-26 15:13:35 +0500

    Apis or Data

---
+++ src/utils/index.js
@@ -0,0 +1,235 @@
+
+
+// Search Tags 
+export const Tags = ["Frontend", "Backend", "Graphic Designer"]
+
+
+// Profile stats 
+
+export const  stats = [
+    { label: "Profile Visitors", value: 140 },
+    { label: "Resume Viewers", value: 20 },
+    { label: "My Jobs", value: 88 },
+  ];
+
+
+// Featured Job Section Data 
+
+ export  const featuredJobs = [
+    {
+      title: "UI/UX Designer",
+      company: "Teams",
+      location: "Seattle, USA (Remote)",
+      timeAgo: "1 day ago",
+      applicants: 22,
+      promoted: true,
+    },
+    {
+      title: "UI/UX Designer",
+      company: "Teams",
+      location: "Seattle, USA (Remote)",
+      timeAgo: "1 day ago",
+      applicants: 22,
+      promoted: true,
+    },
+    {
+      title: "UI/UX Designer",
+      company: "Teams",
+      location: "Seattle, USA (Remote)",
+      timeAgo: "1 day ago",
+      applicants: 22,
+      promoted: true,
+    },
+    {
+      title: "UI/UX Designer",
+      company: "Teams",
+      location: "Seattle, USA (Remote)",
+      timeAgo: "1 day ago",
+      applicants: 22,
+      promoted: true,
+    },
+  
+    {
+      title: "UI/UX Designer",
+      company: "Teams",
+      location: "Seattle, USA (Remote)",
+      timeAgo: "1 day ago",
+      applicants: 22,
+      promoted: true,
+    },
+  ];
+
+
+// Recomended Job Section Data
+  export  const recomendedJobs = [
+    {
+      title: "UI/UX Designer",
+      company: "Teams",
+      location: "Seattle, USA (Remote)",
+      timeAgo: "1 day ago",
+      applicants: 22,
+      promoted: false,
+    },
+    {
+      title: "UI/UX Designer",
+      company: "Teams",
+      location: "Seattle, USA (Remote)",
+      timeAgo: "1 day ago",
+      applicants: 22,
+      promoted: false,
+    },
+    {
+      title: "UI/UX Designer",
+      company: "Teams",
+      location: "Seattle, USA (Remote)",
+      timeAgo: "1 day ago",
+      applicants: 22,
+      promoted: false,
+    },
+    {
+      title: "UI/UX Designer",
+      company: "Teams",
+      location: "Seattle, USA (Remote)",
+      timeAgo: "1 day ago",
+      applicants: 22,
+      promoted: false,
+    },
+  
+    {
+      title: "UI/UX Designer",
+      company: "Teams",
+      location: "Seattle, USA (Remote)",
+      timeAgo: "1 day ago",
+      applicants: 22,
+      promoted: false,
+    },
+
+    {
+      title: "UI/UX Designer",
+      company: "Teams",
+      location: "Seattle, USA (Remote)",
+      timeAgo: "1 day ago",
+      applicants: 22,
+      promoted: false,
+    },
+    {
+      title: "UI/UX Designer",
+      company: "Teams",
+      location: "Seattle, USA (Remote)",
+      timeAgo: "1 day ago",
+      applicants: 22,
+      promoted: false,
+    },
+    {
+      title: "UI/UX Designer",
+      company: "Teams",
+      location: "Seattle, USA (Remote)",
+      timeAgo: "1 day ago",
+      applicants: 22,
+      promoted: false,
+    },
+    {
+      title: "UI/UX Designer",
+      company: "Teams",
+      location: "Seattle, USA (Remote)",
+      timeAgo: "1 day ago",
+      applicants: 22,
+      promoted: false,
+    },
+  
+    {
+      title: "UI/UX Designer",
+      company: "Teams",
+      location: "Seattle, USA (Remote)",
+      timeAgo: "1 day ago",
+      applicants: 22,
+      promoted: false,
+    },
+  ];
+
+// Latest Job Section Data
+  export  const latestJobs = [
+    {
+      title: "UI/UX Designer",
+      company: "Teams",
+      location: "Seattle, USA (Remote)",
+      timeAgo: "1 day ago",
+      applicants: 22,
+      promoted: false,
+    },
+    {
+      title: "UI/UX Designer",
+      company: "Teams",
+      location: "Seattle, USA (Remote)",
+      timeAgo: "1 day ago",
+      applicants: 22,
+      promoted: false,
+    },
+    {
+      title: "UI/UX Designer",
+      company: "Teams",
+      location: "Seattle, USA (Remote)",
+      timeAgo: "1 day ago",
+      applicants: 22,
+      promoted: false,
+    },
+    {
+      title: "UI/UX Designer",
+      company: "Teams",
+      location: "Seattle, USA (Remote)",
+      timeAgo: "1 day ago",
+      applicants: 22,
+      promoted: false,
+    },
+  
+    {
+      title: "UI/UX Designer",
+      company: "Teams",
+      location: "Seattle, USA (Remote)",
+      timeAgo: "1 day ago",
+      applicants: 22,
+      promoted: false,
+    },
+
+    {
+      title: "UI/UX Designer",
+      company: "Teams",
+      location: "Seattle, USA (Remote)",
+      timeAgo: "1 day ago",
+      applicants: 22,
+      promoted: false,
+    },
+    {
+      title: "UI/UX Designer",
+      company: "Teams",
+      location: "Seattle, USA (Remote)",
+      timeAgo: "1 day ago",
+      applicants: 22,
+      promoted: false,
+    },
+    {
+      title: "UI/UX Designer",
+      company: "Teams",
+      location: "Seattle, USA (Remote)",
+      timeAgo: "1 day ago",
+      applicants: 22,
+      promoted: false,
+    },
+    {
+      title: "UI/UX Designer",
+      company: "Teams",
+      location: "Seattle, USA (Remote)",
+      timeAgo: "1 day ago",
+      applicants: 22,
+      promoted: false,
+    },
+  
+    {
+      title: "UI/UX Designer",
+      company: "Teams",
+      location: "Seattle, USA (Remote)",
+      timeAgo: "1 day ago",
+      applicants: 22,
+      promoted: false,
+    },
+  ];
