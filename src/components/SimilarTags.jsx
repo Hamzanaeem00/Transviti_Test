5c1cdaed77a88fdf1d57a75c7c4f62d98f4c9e5b
Hamzanaeem00 <hamzanaeemkhan977@gmail.com>
2025-04-27 16:43:41 +0500

    responsiveness issue

--- src/components/SimilarTags.jsx
+++ src/components/SimilarTags.jsx
@@ -4,15 +4,17 @@ import { Tags } from '../utils'
 const SimliarTags = () => {
   return (
       <div className="mt-4">
-        <span className="text-sm text-gray-600 mr-2">Similar:</span>
+        <div className='flex justify-start '>
+        <span className="text-sm text-gray-600 mr-2 sm:mt-2 md:mt-4 lg:mt-2 xl:mt-2 mt-2">Similar:</span>
         {Tags.map((tag, i) => (
           <button
             key={i}
-            className="px-3 py-2 border border-gray-300 text-sm rounded text-gray-500 mr-2 hover:bg-primary hover:text-white cursor-pointer"
-          >
+            className=" px-3 py-2 border border-gray-300 text-sm rounded text-gray-500 mr-2 hover:bg-primary hover:text-white cursor-pointer"
+            >
             {tag}
           </button>
         ))}
+        </div>
       </div>
   )
 }
